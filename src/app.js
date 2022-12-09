d10d23c50c1b8f2654d4d42596c5c998ef5c5cd1
gfayecorrea <gqcorrea@up.edu.ph>
2022-12-09 08:22:10 +0000

    feat: add get many blogs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import Fastify from 'fastify';
 import { general } from './services/general/index.js';
 import { createBlog } from './services/blogs/create-blog.js';
+import { getManyBlog } from './services/blogs/get-many-blog.js';
 
 const prefix = '/api';
 
@@ -10,7 +11,11 @@ export async function build () {
 
   fastify.get(prefix, general);
 
+  // create blog
   fastify.post(`${prefix}/blog`, createBlog);
+
+  // get many blog
+  fastify.get(`${prefix}/blog`, getManyBlog);
 
   return fastify;
 }
