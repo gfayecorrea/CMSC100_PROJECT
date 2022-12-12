41c448d5b2a8597f124862eb5f422349b3d06997
gfayecorrea <gqcorrea@up.edu.ph>
2022-12-12 04:55:09 +0000

    feat: update to add security on some paths

--- src/services/blogs/update-blog.js
+++ src/services/blogs/update-blog.js
@@ -1,10 +1,21 @@
 import { getDB, saveDB } from '../../utils/db/index.js';
 
 export const updateBlog = async (request, reply) => {
-  const { params, body } = request;
+  const { params, body, username } = request;
   const { blogId: id } = params;
   const { title, description, isDone = null } = body;
+
+  // check if there is username (meaning logged in)
+  if (!username) {
+    return reply.badRequest('Sorry, you are not logged in.');
+  }
+
   const db = await getDB();
+
+  // check if the username logged in is the same as the username saved on the blog to delete
+  if (db.todos[id].username !== username) {
+    return reply.forbidden('Sorry, you are not the owner of this blog.');
+  }
 
   db.blogs[id].title = title || db.blogs[id].title;
   db.blogs[id].description = description || db.blogs[id].description;
