b545ad3a6540d15ffa6ed5965231bfe518fa394d
gfayecorrea <gqcorrea@up.edu.ph>
2022-12-17 08:00:03 +0000

    chore: fix some typo in documentation

--- src/services/blogs/get-blog.js
+++ src/services/blogs/get-blog.js
@@ -17,7 +17,7 @@ export const getBlog = async (request, reply) => {
     return reply.notFound('Blog not found.');
   }
 
-  // check if the username logged in is the same as the username saved on the blog to delete
+  // check if the username logged in is the same as the username saved on the blog to get
   if (db.blogs[id].username !== username) {
     return reply.forbidden('Sorry, you are not the owner of this blog.');
   }
