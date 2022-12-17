6751aea0d6a10cbbc26aeb9ea1a1ff09783ed24b
gfayecorrea <gqcorrea@up.edu.ph>
2022-12-17 07:58:27 +0000

    feat: add ability to update user data

---
+++ src/services/user/update-userdata.js
@@ -0,0 +1,26 @@
+import { getDB, saveDB } from '../../utils/db/index.js';
+
+export const updateUser = async (request, reply) => {
+  const { params, body, username } = request;
+  const { userId: id } = params;
+  const { userName, firstName, lastName } = body;
+
+  // check if there is username (meaning logged in)
+  if (!username) {
+    return reply.badRequest('Sorry, you are not logged in.');
+  }
+
+  const db = await getDB();
+
+  db.users[id].username = userName || db.users[id].username;
+  db.users[id].firstName = firstName || db.users[id].firstName;
+  db.users[id].lastName = lastName || db.users[id].lastName;
+  db.users[id].updatedDate = new Date().getTime();
+
+  await saveDB(db);
+
+  return {
+    id,
+    ...db.users[id]
+  };
+};
