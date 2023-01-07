4f2ef7de4488ca374f1b698fefc2c76a09d71e54
gfayecorrea <gqcorrea@up.edu.ph>
2023-01-07 16:22:52 +0000

    feat: add view user page

---
+++ src/frontend/pages/page-viewuser/index.js
@@ -0,0 +1,42 @@
+import { customElement, property } from 'lit/decorators.js';
+import { state } from '../../worker/index.js';
+import { LitPage } from '../../utils/lit-page/index.js';
+import { template } from './template.js';
+
+/**
+ * @type {LitPage}
+ */
+@customElement('page-viewuser')
+class Page extends LitPage {
+  @property({ type: Object })
+  userData = {};
+
+  @property({ type: String })
+  errorMessage = ''
+
+  render () {
+    return template.bind(this)();
+  }
+
+  async getUser (event) {
+    event.preventDefault();
+    // gets the event.target and change the variable name to form
+    const { target: form } = event;
+
+    // console.log(Object.keys(this.userData).length);
+    const userId = form.username.value;
+
+    const response = await window.fetch(`/api/user/${userId}`);
+
+    // console.log(await response.json());
+
+    if (response.status === 200) {
+      this.errorMessage = '';
+      this.userData = await response.json();
+    }
+    const { message, error } = await response.json();
+    this.errorMessage = `HTTP Code: ${response.status} - ${error} - ${message}`;
+  }
+}
+
+export { Page };
---
+++ src/frontend/pages/page-viewuser/template.js
@@ -0,0 +1,50 @@
+import { html } from 'lit';
+
+export function template () {
+  return html`
+    <style>
+      .blog {
+        display: flex;
+        align-items: center;
+        padding: 15px;
+      }
+      .blog * {
+        flex: 1;
+      }
+    </style>
+    <h2>
+      See User Profile
+    </h2>
+    <form @submit=${this.getUser}>
+    ${this.errorMessage
+      // if this is an errorMessage
+      ? html`
+        <div class="flex-group">
+          ${this.errorMessage}
+        </div>
+      `
+      : ''}
+    <div class="label-input-group">
+      <label for="username">
+        Enter Username:
+      </label>
+      <input type="text" placeholder="username" id="username" name="username">
+    </div>
+    <div class="flex-group">
+      <button>
+        View User
+      </button>
+    </div>
+    ${Object.keys(this.userData).length
+        ? html`
+        <h4> Username : ${this.userData.username}</h4>
+        <h4> First Name : ${this.userData.firstName}</h4>
+        <h4> Last Name : ${this.userData.lastName}</h4>
+        <h4> Created Date : ${new Date(this.userData.createdDate).toDateString()}</h4>
+        <h4> Latest Update Date : ${new Date(this.userData.updatedDate).toDateString()}</h4>
+        `
+        : ''
+    }
+  </form>
+  `;
+}
