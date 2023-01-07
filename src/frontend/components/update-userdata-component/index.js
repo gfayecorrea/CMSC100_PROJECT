3aacecc5633a72ec928070e2a6e00e537c12e918
gfayecorrea <gqcorrea@up.edu.ph>
2023-01-07 16:23:55 +0000

    feat: add page and components for update userdata

---
+++ src/frontend/components/update-userdata-component/index.js
@@ -0,0 +1,45 @@
+import { customElement, property } from 'lit/decorators.js';
+import { LitNoShadow } from '../../utils/lit-no-shadow/index.js';
+import { template } from './template.js';
+
+/**
+ * @type {LitPage}
+ */
+@customElement('update-userdata-component')
+class Component extends LitNoShadow {
+  @property({ type: String })
+  errorMessage = ''
+  render () {
+    return template.bind(this)();
+  }
+  // this is called when submit button is clicked (see template.js)
+  async updateUserData (event) {
+    // this prevents the page from using the default behavior
+    // of form submit
+    event.preventDefault();
+    // gets the event.target and change the variable name to form
+    const { target: form } = event;
+    const userId = form.username.value;
+    const firstName = form.firstName.value;
+    const lastName = form.lastName.value;
+    // calls an API call
+    const response = await window.fetch(`/api/user/${userId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName
+      })
+    });
+
+    if (response.status === 200) {
+        this.errorMessage = '';
+    }
+    const { message, error } = await response.json();
+    this.errorMessage = `HTTP Code: ${response.status} - ${error} - ${message}`;
+    }
+}
+
+export { Component };
---
+++ src/frontend/pages/page-update-userdata/template.js
@@ -0,0 +1,7 @@
+import { html } from 'lit';
+// loads the update-userdata-component
+import '../../components/update-userdata-component/index.js';
+
+export function template () {
+  return html`<update-userdata-component></update-userdata-component>`;
+}
